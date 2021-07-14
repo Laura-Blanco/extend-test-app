fcfacdd2bc0933de0aebb0a6ca665d1218e5e455
Laura Blanco <laurablanco@Extends-MBP.attlocal.net>
2021-07-14 12:58:51 -0600

    Created a shopping list

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# Extend test app 
+
+This repository will be used as a practice for learning git commands and updating code. 
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState} from 'react';
+
+function App() {
+  const [list,setList] = useState([]);
+  const [inputData, setInputData] = useState('');
+  const handleAddItem = () => {
+    const newList = [...list, {title: inputData}];
+    setList(newList);
+    setInputData('');
+    console.log(list)
+  }
+
+  return (
+    <div className = "App">
+      <h1> Laura's Shopping List </h1>
+      <div className = "Shopping Items">
+        <input type = "text" value = {inputData} onChange={(event) => setInputData(event.target.value)} />
+        <button onClick = {() => handleAddItem()}>
+          Enter
+        </button>  
+      </div>
+      <div className = 'list'>
+        {list.map((item,index) => {
+          return (
+            <div>
+              <p> {item.title}</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;

af89ab70c290e806244dc0774018c3ae7191a59a
Laura Blanco <laurablanco@Extends-MBP.attlocal.net>
2021-07-16 09:10:48 -0600

    route changes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
-import { Route , Link} from 'react-router-dom';
+import { Route , Link, Router} from 'react-router-dom';
 import { render } from '@testing-library/react';
 import Lists from './lists';
-import creteList from './createList';
+import createList from './createList';
 
-function App() {
-  const [list,setList] = useState([]);
-  const [inputData, setInputData] = useState('');
-  const handleAddItem = () => {
-    const newList = [...list, {title: inputData}];
-    setList(newList);
-    setInputData('');
-    console.log(list)
-  }
-
-  return (
-    <div className = "App">
-      <h1> Laura's Shopping List </h1>
-      <div className = "Shopping Items">
-        <input type = "text" value = {inputData} onChange={(event) => setInputData(event.target.value)} />
-        <button onClick = {() => handleAddItem()}>
-          Enter
-        </button>  
-      </div>
-      <div className = 'list'>
-        <Route path = "/" component={Lists}/>
-        {list.map((item,index) => {
-          return (
-            <div>
-              <p> {item.title}</p>
-            </div>
-          )
-        })}
-      </div>
-    </div>
+export default function App(){
+  return(
+    <Router>
+      <Route path="/" component={createList}/>
+    </Router>
   );
 }
-
-export default App;
